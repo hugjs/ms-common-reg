061b1e3abbeb712ebb2a4fba27d56d8e61616ad3
peterzhou84 <zjzhoufy@126.com>
2017-11-06 18:10:09 +0800

    初始化服务节点的数据

--- src/zk/node.js
+++ src/zk/node.js
@@ -10,6 +10,11 @@ var Util    = require('util');
 
 var _       = require('lodash');
 var cuid = require('cuid');
+
+var zookeeper = require('node-zookeeper-client');
+const internalIp = require('internal-ip');
+
+var client = zookeeper.createClient(config.get("service_node.storage.options.zk.url"));
 /**
  * 全局的事件通过这个事件管理对象管理
  * 事件的数据，会包含相对多的信息
@@ -58,6 +63,31 @@ async function Node(options) {
     }
     this._id = cuid();
     this._status = SERVICE_STATUS.INIT;
+    this._url = "";
+    this._app = config.get("service_node.base.app");
+    this._app_version = config.get("service_node.base.app_version");
+    this._service = config.get("service_node.base.service");
+    this._version = config.get("service_node.base.service_version");
+    this._enabled = 0;
+    this._server = config.get("service_node.server");
+    if(!this._server.protocal) this._server.protocal = "http";
+    if(!this._server.host){
+        this._server.host = internalIp.v4.sync();
+    }
+    if(!this._server.port){
+        switch(this._server.protocal){
+            case 'https':
+                this._server.port = 443;
+                break;
+            default:
+                this._server.port = 80;
+        }
+    }
+    if(!this._server.root) this._server.root = "/";
+    this._url = `${this._server.protocal}://${this._server.host}:${this._server.port}${this._server.root}`;
+    await this.connect();
+    await this.regToPool();
+    await this.reg();
     return this;
 }
 
@@ -76,9 +106,46 @@ Node.prototype.status = function(){
 }
 
 /**
+ *  Connect to the zk server
+ */
+Node.prototype.connect = function(){
+    var self = this;
+    return new Promise((resolve, reject) => {
+        var resoled = false;
+        client.connect();
+        client.once('connected',()=>{
+            resolve();
+        });
+        client.once('disconnected',()=>{
+            // 断开连接之后自动重连
+            this.connect();
+        });
+        var timeout = config.get("service_node.storage.options.zk.timeout");
+        setTimeout(function(){
+            (client && client.getState() == zookeeper.State.SYNC_CONNECTED) ||  reject();
+        },timeout?timeout:3000)
+      });
+}
+
+/**
+ * 注册到服务池
+ */
+Node.prototype.regToPool = async function(){
+    client.mkdirp(
+        self._path, new Buffer(JSON.stringify({
+            url: self._url,
+            enabled: 0,
+            version: self._version
+        })),
+        zookeeper.CreateMode.EPHEMERAL, function(err, p){
+        
+    })
+}
+
+/**
  * 注册到服务注册树
  */
-Node.prototype.reg = function(){
+Node.prototype.reg = async function(){
 
 }
 
