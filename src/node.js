14645dd806bc343aee5726b2f32c9b99a05ec8a0
peterzhou84 <zjzhoufy@126.com>
2017-11-07 18:10:17 +0800

    代码结构调整，把公共代码提取到父类代码中

--- src/node.js
+++ src/node.js
@@ -1,14 +1,28 @@
 /**
  * 服务节点相关服务入口
  */
+var config = require('config')
 var noop = function(){}
 var path = require('path');
-const logger = require('@log4js-node/log4js-api').getLogger(path.basename(module.id));
+const logger = require('@log4js-node/log4js-api').getLogger("servicenode");
 var Events  = require('events');
 var Util    = require('util');
 
 var _       = require('lodash');
 var cuid = require('cuid');
+const internalIp = require('internal-ip');
+
+
+// Service Status
+const SERVICE_STATUS = {
+    INIT : 0,          // 初始化状态
+    STANDBY : 1,       // 服务就绪，手动设置为disable的时候也是这个状态
+    ENABLE : 2,        // 激活
+  };
+
+var rootInCfg = config.get("service_node.storage.options.root")
+const ROOT = rootInCfg?rootInCfg:"/MICRO/services";
+
 /**
  * 全局的事件通过这个事件管理对象管理
  * 事件的数据，会包含相对多的信息
@@ -18,13 +32,6 @@ var eventbus = new Events.EventEmitter();
 
 Util.inherits(Node, Events.EventEmitter);
 
-// Service Status
-const SERVICE_STATUS = {
-    INIT : 0,          // 初始化状态
-    STANDBY : 1,       // 服务就绪，手动设置为disable的时候也是这个状态
-    ENABLE : 2,        // 激活
-  };
-  
 
 /**
  * 初始化服务列表对象，如果已经存在就直接返回
@@ -42,11 +49,50 @@ exports.init = function(options){
  * @param {Object} options 
  */
 function Node(options) {
+}
+
+Node.prototype.init = function(options){
     Events.EventEmitter.call(this);
+    // 数据校验
+    var keys = ['app','app_version','service','service_version'];
+    if(_.pick(_.omitBy(options,_.isNil),keys).length < keys.length){
+        throw new Error(100000,"参数错误")
+    }
     this._id = cuid();
-    this._status = SERVICE_STATUS.STANDBY;
+    this._status = SERVICE_STATUS.INIT;
+    this._url = "";
+    this._app = config.get("service_node.basic.app");
+    this._app_version = config.get("service_node.basic.app_version");
+    this._service = config.get("service_node.basic.service");
+    this._version = config.get("service_node.basic.service_version");
+    this._enabled = 0;
+    this._path = ROOT + "/" + this._app;
+    this._server = config.get("service_node.server");
+    if(!this._server.protocal) this._server.protocal = "http";
+    if(!this._server.host){
+        this._server.host = internalIp.v4.sync();
+    }
+    if(!this._server.port){
+        switch(this._server.protocal){
+            case 'https':
+                this._server.port = 443;
+                break;
+            default:
+                this._server.port = 80;
+        }
+    }
+    if(!this._server.root) this._server.root = "/";
+    this._url = `${this._server.protocal}://${this._server.host}:${this._server.port}${this._server.root}`;
+    logger.debug('NODE DATA: ', JSON.stringify(this))
+    var prms = this.connect();
+    prms.then(this.regToPool.bind(this))
+        .then(this.reg.bind(this))
+        .catch((reason) => {
+            logger.error('New Node failed: %s', JSON.stringify(reason));
+        });
     return this;
 }
+
 
 /**
  * 获得服务节点的id
@@ -62,9 +108,34 @@ Node.prototype.status = function(){
     return this._status;
 }
 
+Node.prototype.connect = function(){
+    logger.debug('connect')
+    var self = this;
+    
+    return new Promise((resolve, reject) => {
+        logger.debug('connect in promise')
+    });
+}
 
+Node.prototype.regToPool = function(){
+    logger.debug('regToPool')
+    var self = this;
+    
+    return new Promise((resolve, reject) => {
+        logger.debug('regToPool in promise')
+    });
+}
 
-module.exports = Node;
+Node.prototype.reg = function(){
+    logger.debug('reg')
+    var self = this;
+    
+    return new Promise((resolve, reject) => {
+        logger.debug('reg in promise')
+    });
+}
+module.exports = Node
+
 _.forEach(SERVICE_STATUS, function (key) {
   module.exports[key] = SERVICE_STATUS[key];
 });
--- src/zk/node.js
+++ src/zk/node.js
@@ -7,13 +7,11 @@ var path = require('path');
 const logger = require('@log4js-node/log4js-api').getLogger("zk.node");
 var Events  = require('events');
 var Util    = require('util');
-
 var _       = require('lodash');
-var cuid = require('cuid');
 
 var zookeeper = require('node-zookeeper-client');
-const internalIp = require('internal-ip');
 
+var pNode = require('../node');
 var client = zookeeper.createClient(config.get("service_node.storage.options.zk.url"));
 /**
  * 全局的事件通过这个事件管理对象管理
@@ -24,15 +22,8 @@ var eventbus = new Events.EventEmitter();
 
 Util.inherits(Node, Events.EventEmitter);
 
-// Service Status
-const SERVICE_STATUS = {
-    INIT : 0,          // 初始化状态
-    STANDBY : 1,       // 服务就绪，手动设置为disable的时候也是这个状态
-    ENABLE : 2,        // 激活
-  };
+Util.inherits(Node, pNode);
 
-var rootInCfg = config.get("service_node.storage.options.root")
-const ROOT = rootInCfg?rootInCfg:"/MICRO/services";
 
 /**
  * 服务节点对象，如果已经存在就直接返回
@@ -57,45 +48,8 @@ exports.init = async function(options){
  * @param {Object} options 
  */
 function Node(options) {
-    Events.EventEmitter.call(this);
-    // 数据校验
-    var keys = ['app','app_version','service','service_version'];
-    if(_.pick(_.omitBy(options,_.isNil),keys).length < keys.length){
-        throw new Error(100000,"参数错误")
-    }
-    this._id = cuid();
-    this._status = SERVICE_STATUS.INIT;
-    this._url = "";
-    this._app = config.get("service_node.basic.app");
-    this._app_version = config.get("service_node.basic.app_version");
-    this._service = config.get("service_node.basic.service");
-    this._version = config.get("service_node.basic.service_version");
-    this._enabled = 0;
-    this._path = ROOT + "/" + this._app;
-    this._server = config.get("service_node.server");
-    if(!this._server.protocal) this._server.protocal = "http";
-    if(!this._server.host){
-        this._server.host = internalIp.v4.sync();
-    }
-    if(!this._server.port){
-        switch(this._server.protocal){
-            case 'https':
-                this._server.port = 443;
-                break;
-            default:
-                this._server.port = 80;
-        }
-    }
-    if(!this._server.root) this._server.root = "/";
-    this._url = `${this._server.protocal}://${this._server.host}:${this._server.port}${this._server.root}`;
-    logger.debug('NODE DATA: ', JSON.stringify(this))
-    var prms = this.connect();
-    prms.then(this.regToPool.bind(this))
-        .then(this.reg.bind(this))
-        .catch((reason) => {
-            logger.error('New Node failed: %s', JSON.stringify(reason));
-        });
-    return this;
+    this.init.bind(this)(options);
+    return this
 }
 
 /**
@@ -170,19 +124,4 @@ Node.prototype.regToPool = function(){
     });
 }
 
-/**
- * 注册到服务注册树
- */
-Node.prototype.reg = function(){
-    logger.debug('reg')
-    var self = this;
-    return new Promise((resolve, reject) => {
-        resolve();
-    });
-
-}
-
-_.forEach(SERVICE_STATUS, function (key) {
-  module.exports[key] = SERVICE_STATUS[key];
-});
 
